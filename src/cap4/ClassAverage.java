5759304b5002a17a10ff85ac3f2c8503bde3d9a5
Clayton <clayton_monson@hotmail.com>
2023-12-11 13:42:28 -0300

    criacao da classe ClassAverage

---
+++ src/cap4/ClassAverage.java
@@ -0,0 +1,34 @@
+package cap4;
+
+import java.util.Scanner;
+
+public class ClassAverage {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 0;
+        int gradeCounter = 0;
+
+        System.out.print("Enter grade or -1 to quit: ");
+        int grade = sc.nextInt();
+
+        while (grade != -1) {
+            total += grade;
+            gradeCounter++;
+            System.out.print("Enter grade or -1 to quit: ");
+            grade = sc.nextInt();
+        }
+
+        if (gradeCounter != 0) {
+            double average = (double) total / gradeCounter;
+            System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+            System.out.printf("Class average is %.2f%n", average);
+        } else {
+            System.out.println("No grades were entered");
+        }
+
+        sc.close();
+    }
+}
